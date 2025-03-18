1ec5df60b053ce8e02f4578d9cf38f8828f63658
Rolf Veen <rolf.veen@gmail.com>
2025-03-18 13:35:25 +0100

    ...

--- upload.go
+++ upload.go
@@ -13,13 +13,12 @@ import (
 )
 
 const (
-	FileDir = "file"
-	TmpDir  = ".tmp"
+	fileDir = "file"
+	tmpDir  = ".tmp"
 )
 
-func init() {
-	//	os.MkdirAll(FileDir, 0644)
-	os.MkdirAll(TmpDir, 0644)
+func CreateUploadDir() {
+	os.MkdirAll(tmpDir, 0644)
 }
 
 func fileUpload(r *http.Request, user string) (*ogdl.Graph, error) {
@@ -30,8 +29,6 @@ func fileUpload(r *http.Request, user string) (*ogdl.Graph, error) {
 	if err != nil {
 		return nil, err
 	}
-
-	folder := TmpDir
 
 	buf := make([]byte, 1000000)
 
@@ -59,14 +56,13 @@ func fileUpload(r *http.Request, user string) (*ogdl.Graph, error) {
 				continue
 			}
 
-			log.Println("uploading:", folder+"/"+v.Filename)
+			log.Println("uploading:", tmpDir+"/"+v.Filename)
 
-			wfile, err = os.Create(folder + "/" + v.Filename)
+			wfile, err = os.Create(tmpDir + "/" + v.Filename)
 			if err != nil {
 				log.Println(err)
 				return nil, err
 			}
-			defer wfile.Close()
 
 			h := md5.New()
 			for {
@@ -81,14 +77,16 @@ func fileUpload(r *http.Request, user string) (*ogdl.Graph, error) {
 				}
 			}
 
-			fname := FileDir + "/" + hex.EncodeToString(h.Sum(nil)) + ext
+			wfile.Close()
+
+			fname := fileDir + "/" + hex.EncodeToString(h.Sum(nil)) + ext
 
 			log.Println("uploading file with MD5", hex.EncodeToString(h.Sum(nil)))
 			log.Println("moving to", fname)
-			err = os.Rename(folder+"/"+v.Filename, fname)
+			err = os.Rename(tmpDir+"/"+v.Filename, fname)
 			if err != nil {
 				cwd, _ := os.Getwd()
-				log.Printf("cwd %s src dir %s dest dir %s\n", cwd, folder+"/"+v.Filename, fname)
+				log.Printf("cwd %s src dir %s dest dir %s\n", cwd, tmpDir+"/"+v.Filename, fname)
 				log.Println(err)
 			}
 
